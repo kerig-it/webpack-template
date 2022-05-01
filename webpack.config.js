3fb44622937f539a90a183def42db7db5cce6af3
msfninja <msfninja@proton.me>
2022-05-01 15:35:03 +0200

    Update `webpack.config.js`
    This commit changes quite what. What's been changed:
    
     - Component and Markdown conversion management libraries have been added
     - A different way of generating HTML from templates has been implemented
     - The publicPath property has been added in the output property of the export object to avoid relative paths in linked resources in HTML code

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
-// Webpack configuration file.
+/*
+ * This is the Webpack configuration file.
+ *
+ * Refer to the README in this repository's root for more
+ * information.
+*/
 
-// Modules
+// Packages/libraries
 const
 	fs = require('fs'),
 	HtmlWebpackPlugin = require('html-webpack-plugin'),
 	path = require('path');
+
+const
+	components = require(path.join(
+		__dirname, 'lib', 'components'
+	)),
+	converter = require(path.join(
+		__dirname, 'lib', 'converter'
+	));
 
 let config; // Configuration object --> ./config.json
 
@@ -17,26 +30,62 @@ catch (error) {
 	throw error;
 }
 
+// Set the configuration object for the components handler.
+components.set(config);
+
 // Add your plugins in the below array, not in the actual export
 // object.
-let plugins = [
-	// Default page
-	new HtmlWebpackPlugin({
-		filename: 'index.html',
-		meta: { viewport: 'width=device-width, initial-scale=1.0' },
-		template: './src/default.html',
-		title: config.name
-	})
-];
+let plugins = [];
 
-fs.readdirSync(path.join(__dirname, 'src', 'pages')).forEach(file => {
-	if (file.match(/\.x?html?/i)) {
+// Object literal with regular expressions for relevant extensions
+let exts = {
+	html: /\.html?$/i,
+	md: /\.m(ark)?d(own)?$/i
+};
+
+// Loop over all page files.
+for (const file of fs.readdirSync(
+	path.join(__dirname, 'src', 'pages')
+)) {
+	let
+		content, ext, meta,
+		// Path to target file
+		pathname = path.join(__dirname, 'src', 'pages', file);
+
+	// Are we dealing with Markdown?
+	if (exts.md.test(file)) {
+		// Convert Markdown to HTML.
+		content = converter.convert(pathname);
+		ext = 'md';
+		meta = converter.meta(pathname);
+	}
+	else if (exts.html.test(file)) {
+		content = fs.readFileSync(pathname).toString();
+		ext = 'html';
+	}
+
+	if (content) {
+		let base = fs.readFileSync(path.join(
+			__dirname, 'src', 'default.html'
+		)).toString();
+
+		content = components.insert(
+			base.replace('{{content}}', content), meta
+		);
+
+		let page = file.replace(exts[ext], '');
+
 		plugins.push(new HtmlWebpackPlugin({
-			filename: `pages/${file}`,
-			template: `./src/pages/${file}`
+			filename: path.join(
+				page === 'home' ? '' : page, 'index.html'
+			),
+			inject: 'body',
+			meta: config.meta,
+			templateContent: content,
+			title: `${page} | ${config.name}`
 		}));
 	}
-});
+}
 
 module.exports = {
 	entry: './src/index.js',
@@ -44,31 +93,19 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.s?[ac]ss$/i,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				]
+				test: /\.s[ac]ss$/i,
+				use: [ 'style-loader', 'css-loader', 'sass-loader' ]
 			},
-			{
-				test: /\.((pn|jpe?|sv)g|[gpt]if)$/i,
-				type: 'asset/resource'
-			},
-			{
-				test: /\.((eo|[ot])tf?|woff2?)$/i,
-				type: 'asset/resource',
-			},
-			{
-				test: /\.(x?html?)/i,
-				use: 'raw-loader'
-			}
+			{ test: /\.((pn|jpe?|sv)g|[gt]if)$/i, type: 'asset/resource' },
+			{ test: /\.((eo|[ot])tf?|woff2?)$/i, type: 'asset/resource' },
+			{ test: /\.html?$/i, use: 'raw-loader' }
 		]
 	},
 	output: {
 		filename: 'bundle.js',
 		hashFunction: 'xxhash64',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/'
 	},
 	plugins: plugins
 };
