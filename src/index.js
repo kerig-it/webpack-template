22007e8e36b821c85f521e1d82e85dcdf1914353
msfninja <msfninja@proton.me>
2022-05-01 15:30:34 +0200

    Update `index.js`
    This commit switches from the manual without-page-reloads surfing to the Pjax library.

--- src/index.js
+++ src/index.js
@@ -1,61 +1,16 @@
 /*
- * webpack-tmpl—A template repository for webpack applications.
+ * webpack-template—A template repository for webpack applications.
  *
  * Refer to the README in this repository's root for more
  * information.
  *
- * GitHub: https://github.com/kerig-it/webpack-tmpl
+ * GitHub: https://github.com/kerig-it/webpack-template
  *
  * Made with ❤️ by Kerig.
 */
 
-// Assets/resources
-import config from '../config.json';
+import Pjax from 'pjax/pjax.js';
 
-// Loads a page
-const goto = async pathname => {
-
-	// Await a fetch request.
-	await fetch(
-		`/pages/${pathname === '/' ? 'home' : pathname.replace(/^\/*/, '')}.html`,
-		{ method: 'GET' }
-	)
-	.then(response => response.text())
-	.then(data => {
-
-		// Display the fetched data.
-		document.body.innerHTML = data;
-
-		// Update the history object.
-		window.history.pushState(
-			{ html: data },
-			'',
-			pathname
-		);
-	})
-	.catch(error => {
-		// If there was an error, throw it.
-		throw error;
-	});
-};
-
-// Main function
-const main = () => {
-	'use strict';
-	
-	goto(window.location.pathname);
-
-	window.onpopstate = event => {
-		if (event.state) {
-			document.body.innerHTML = event.state.html;
-		}
-	};
-};
-
-try /*one's luck*/ {
-	main();
-}
-catch (error) {
-	// Throw errors.
-	throw error;
-}
+const pjax = new Pjax({
+	selectors: [ '*[data-pjax=true]' ]
+});
